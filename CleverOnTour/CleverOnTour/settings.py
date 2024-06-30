1553bacd7455fd818b1d34754cc346bc3db7d836
estebanquito123 <estebantoro832@gmail.com>
2024-06-30 18:25:32 -0400

    ajustes

--- CleverOnTour/CleverOnTour/settings.py
+++ CleverOnTour/CleverOnTour/settings.py
@@ -125,3 +125,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL="/"
 LOGOUT_REDIRECT_URL="/"
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
