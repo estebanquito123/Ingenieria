6600c39fc65eacf1b190ec47112f930f5942e073
estebanquito123 <estebantoro832@gmail.com>
2024-06-30 18:12:42 -0400

    Inicio de sesion

--- CleverOnTour/OnTour/urls.py
+++ CleverOnTour/OnTour/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('index/servicios', views.servicios, name='servicios'),
     path('index/nosotros', views.nosotros, name='nosotros'),
-    path('index/login', views.login, name='login'),
+    path('login/', views.user_login, name='login'),
     path('servicios/imperdibles', views.login, name='imperdibles'),
     path('servicios/aprendizaje', views.login, name='aprendizaje'),
+    path('logout/',views.exit, name='exit'),
+    path('index/perfil', views.perfil, name='perfil'),
 ]
--- CleverOnTour/OnTour/views.py
+++ CleverOnTour/OnTour/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
 
 # Create your views here.
 def index(request):
@@ -13,9 +17,21 @@ def nosotros(request):
     context={}
     return render(request,'OnTour/nosotros.html', context)
 
-def login(request):
+def perfil(request):
     context={}
-    return render(request,'OnTour/login.html', context)
+    return render(request,'OnTour/perfil.html', context)
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')  # Redirige a la página de inicio o a la que desees
+        else:
+            messages.error(request, 'Usuario o contraseña incorrectos')
+    return render(request, 'login.html')
 
 def aprendijzaje(request):
     context={}
@@ -24,3 +40,7 @@ def aprendijzaje(request):
 def imperdibles(request):
     context={}
     return render(request,'OnTour/imperdibles.html', context)
+
+def exit(request):
+    logout(request)
+    return redirect('/')
